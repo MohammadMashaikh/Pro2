600ea61d5b98554ee305ee74526acb73b204344f
Mohammad Mashaikh <mohammadmashaikh@yahoo.com>
2021-11-20 14:01:34 +0200

    Add project files.

---
+++ Pro2/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pro2.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Contact")]
+        public ActionResult cnt()
+        {
+            return View();
+        }
+
+    }
+    
+
+
+
+
+}
+
+
